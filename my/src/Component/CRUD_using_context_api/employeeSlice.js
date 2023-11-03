d76f052436d363179f1524a00922a547ef50b34f
pranaliPukale <113536444+pranaliPukale@users.noreply.github.com>
2023-11-03 18:56:35 +0530

    redux1
    redux1

---
+++ my/src/Component/CRUD_using_context_api/employeeSlice.js
@@ -0,0 +1,25 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const initialState ={
+    employees:[]
+}
+const employeeSlice = createSlice({
+    name:'employee',
+    initialState,
+    reducers:{
+        addEmployee : (employee)=>{
+           
+        },
+        updateEmployee :(id,updatedEmployee)=>{
+           
+        },
+        deleteEmployee : (id)=>{
+        
+       }
+
+    }
+
+})
+export const {addEmployee,updateEmployee,deleteEmployee} = employeeSlice.actions
+export default employeeSlice.reducer
+
